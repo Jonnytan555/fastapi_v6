edb9b0e350d8f2d284195d2348ced461ed121177
Winter1963#december1 <jonnyedwards1@hotmail.co.uk>
2025-11-10 17:05:00 +0000

    Fix user table migration (use create_table)

--- alembic/versions/9c4d41ae246a_add_user_table.py
+++ alembic/versions/9c4d41ae246a_add_user_table.py
@@ -1,36 +1,29 @@
-"""add user table
-
-Revision ID: 9c4d41ae246a
-Revises: 1366c95b72b3
-Create Date: 2025-11-07 11:37:59.790584
-
-"""
-from typing import Sequence, Union
-
 from alembic import op
 import sqlalchemy as sa
 
 
-# revision identifiers, used by Alembic.
-revision: str = '9c4d41ae246a'
-down_revision: Union[str, Sequence[str], None] = '1366c95b72b3'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision = '9c4d41ae246a'
+down_revision = '1366c95b72b3'
+branch_labels = None
+depends_on = None
 
 
 def upgrade() -> None:
-    op.add_column('users', 
-                  sa.Column('id', sa.Integer(), nullable=False),
-                  sa.Column('email', sa.String(), nullable=False), # type: ignore
-                  sa.Column('password', sa.String(), nullable=False),
-                  sa.Column('created_at', sa.TIMESTAMP(timezone=True), 
-                  server_default=sa.text('now()'), nullable=False),
-                  sa.PrimaryKeyConstraint('id'),
-                  sa.UniqueConstraint('email')
-                  )
-    pass
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('email', sa.String(), nullable=False),
+        sa.Column('password', sa.String(), nullable=False),
+        sa.Column(
+            'created_at',
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.text('now()'),
+            nullable=False,
+        ),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('email'),
+    )
 
 
 def downgrade() -> None:
     op.drop_table('users')
-    pass
